eccdf4a49fc8566bc91874e3e7bc79c91923aaf7
sc18am <80413031+sc18am@users.noreply.github.com>
2021-05-06 17:18:05 +0300

    Add files via upload

---
+++ Project/Elective - CSURG/elecquery.pgsql
@@ -0,0 +1,15 @@
+set search_path to mimiciii;
+
+COPY (  
+    SELECT 
+    a.hadm_id, a.admittime, a.dischtime, a.deathtime, a.admission_type, 
+    s.subject_id, s.hadm_id, s.transfertime, s.curr_service,
+    t.hadm_id, t.icustay_id, t.intime, t.outtime, t.curr_wardid, t.curr_careunit
+    FROM
+    services s
+    INNER JOIN admissions a
+        ON s.hadm_id = a.hadm_id
+    INNER JOIN transfers t
+        ON a.hadm_id = t.hadm_id
+    WHERE a.admission_type = 'ELECTIVE' and s.curr_service = 'CSURG'
+) TO 'C:/Users/alexm/OneDrive/Desktop/elective-csurg/elective-csurg.csv' DELIMITER ',' CSV HEADER;
---
+++ Project/Elective - TRAUMA/convert.py
@@ -0,0 +1,79 @@
+import csv
+from csv import DictReader
+import itertools
+
+# cleans and sorts the patients timestamps by case id and what sort of timestamp it is
+def create_list():
+    with open("elective-trauma.csv") as csv_file:
+
+        f = open("elective.txt", "a")
+
+        dict_reader = DictReader(csv_file)
+        
+        for row in dict_reader:    
+
+            if len(row['outtime']) != 0:  
+                
+                
+                admittime = row['hadm_id']  +","+  row['subject_id'] +","+ row['admittime'] +", ADMISSION"
+                f.write(admittime)
+                f.write("\n")
+                
+                
+                if row['transfertime'] == row['intime']:
+                    service =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  +","+  row['curr_service'] 
+                    f.write(service)
+                    f.write("\n")
+
+                if row['transfertime'] == row['intime'] and len(row['curr_careunit']) != 0 and row['curr_careunit'] == "TSICU":
+                    icustay =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  +","+  row['curr_careunit'] 
+                    f.write(icustay)
+                    f.write("\n")
+                
+                
+                if row['transfertime'] == row['intime'] and len(row['curr_careunit']) != 0 and row['curr_careunit'] != "TSICU":
+                    icustay =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  +", OTHER ICU"
+                    f.write(icustay)
+                    f.write("\n")
+
+
+                elif len(row['icustay_id']) == 0 and row['transfertime'] != row['intime'] and len(row['curr_wardid']) != 0:
+                    wardstay =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  + ", WARD"
+                    f.write(wardstay)
+                    f.write("\n")
+
+
+                elif len(row['icustay_id']) != 0 and row['curr_careunit'] == "TSICU":
+                    icustay =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  + "," + row['curr_careunit']  
+                    f.write(icustay)
+                    f.write("\n")
+
+                elif len(row['icustay_id']) != 0 and row['curr_careunit'] != "TSICU":
+                    icustay =  row['hadm_id']  +","+ row['subject_id'] +","+ row['intime']  + ", OTHER ICU" 
+                    f.write(icustay)
+                    f.write("\n")
+
+                dischtime = row['hadm_id']  +","+  row['subject_id'] +","+ row['dischtime'] + ', DISCHARGED'
+                f.write(dischtime)
+                f.write("\n")   
+
+                if len(row['deathtime']) != 0:  
+                    deathtime = row['hadm_id']  +","+  row['subject_id'] +","+ row['deathtime'] + ', DEAD'
+                    f.write(deathtime)
+                    f.write("\n")
+        
+            else:
+                continue
+
+def write_to_csv():
+    
+    with open("elective.txt", "r") as infile:
+        stripped = (line.strip() for line in infile)
+        lines = (line.split(",") for line in stripped if line)
+        with open("event_log.csv", "w") as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(("hadm_id", "subject_id", "timestamp", "event"))
+            writer.writerows(lines)
+
+create_list()
+write_to_csv()
